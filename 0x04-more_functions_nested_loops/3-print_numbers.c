f973d9bec6b847e6ddcfe43587d8fc45eae36fd2
yadi0988 <terefeyadamzer@gmail.com>
2023-01-01 23:37:40 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int 1;
+
+i = 0;
+while (i < 10)
+{
+_putchar (i + '0');
+i++
+}
+
+_putchar ('\n');
+}
